d709af2d106f423919f4efaa6e6a1c5d2e54b7ad
lljmb <brown.jessm@icloud.com>
2020-10-13 23:10:12 -0400

    array methods & callbacks

--- index.js
+++ index.js
@@ -15,31 +15,33 @@ console.log('its working');
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+let getFinals = fifaData.filter((finals) =>{
+    return finals.Stage === 'Final'
+})
 
-    /* code here */
-
-};
+  console.log(getFinals) 
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-
-    /* code here */
-
+function getYears(callback) {
+    let years = getFinals.map((year) => {
+        return year.Year
+      })
+      return years
 };
 
-getYears();
+console.log(getYears(getFinals))
+
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-
+/* function getWinners(callback) {
+    ['Home Team Goals'] >
     /* code here */
 
-};
+// };
 
-getWinners();
+// getWinners(); */
 
 /* Task 5: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 
@@ -56,13 +58,14 @@ getWinnersByYear();
 
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
-function getAverageGoals(/* code here */) {
-
-    /* code here */
-
-};
-
-getAverageGoals();
+const getAverageGoals = fifaData.reduce((total, goal) => {
+    let sum = total + goal['Home Team Goals'] || total + goal['Away Team Goals'];
+    return sum;
+  }, 0)
+  
+  const average = getAverageGoals / fifaData.length;
+  
+  console.log("the average number of goals scored per game is: " +average)
 
 /// STRETCH 🥅 //
 
